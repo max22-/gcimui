ac64772872bd880f25e9e00ea9dcf94cae7a753c
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2024-08-02 21:59:41 +0200

    Corrected backend.c to handle multiple keys at once

--- src/backend.c
+++ src/backend.c
@@ -12,14 +12,14 @@ void fill_rectangle(int x, int y, int w, int h, ui_color color) {
 void handle_keys(ui_ctx *ctx) {
     if(IsKeyDown(KEY_UP))
         ui_set_key(&ctx, UI_KEY_UP);
-    else if(IsKeyDown(KEY_DOWN))
+    if(IsKeyDown(KEY_DOWN))
         ui_set_key(&ctx, UI_KEY_DOWN);
-    else if(IsKeyDown(KEY_LEFT))
+    if(IsKeyDown(KEY_LEFT))
         ui_set_key(&ctx, UI_KEY_LEFT);
-    else if(IsKeyPressed(KEY_RIGHT))
+    if(IsKeyDown(KEY_RIGHT))
         ui_set_key(&ctx, UI_KEY_RIGHT);
-    else if(IsKeyDown(KEY_ENTER))
+    if(IsKeyDown(KEY_ENTER))
         ui_set_key(&ctx, UI_KEY_ENTER);
-    else if(IsKeyDown(KEY_BACKSPACE))
+    if(IsKeyDown(KEY_BACKSPACE))
         ui_set_key(&ctx, UI_KEY_BACK);
 }
